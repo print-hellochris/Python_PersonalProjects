b694852a089c492fc8d9a6da3ce82c9d71cb0e3f
print-hellochris <print.christopherguimaraes@gmail.com>
2022-10-14 11:23:51 -0300

    Documented and added some possible troubleshooting to it

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,29 +1,43 @@
 def arithmetic_arranger(self):
 
+
     for i in self:
-
+        # Removes blank space before continuing
         i = i.replace(" ", "")
-
-        if "+" in i:
+        #Checks if there are more than 5 problems to the function
+        if len(self) > 5:
+            print("Error: Too many problems")
+            break
+        #Checks if there is a division operation in the arguments
+        elif "/" in i:
+            print("Error:Operator must be either '+' or '-' ")
+            break
+        #checks if there is a multiplication operation in the arguments
+        elif "*" in i:
+            print("Error:Operator must be either '+' or '-' ")
+            break
+        #Checks if there is a + operator.
+        elif "+" in i:
             n = i.index("+")
+            # Separates operand1 and operand2 based on the index of the operator
             num1 = int(i[0:n])
             num2 = int(i[n + 1:])
-            result = num1 + num2
-
+            # Prints the equation
             print(' ', num1, '\n', '+', num2, '\n______\n ', num1 + num2, "\n", sep='')
-
-            # return " "
-
+        # Checks if there is a - operator.
         elif "-" in i:
+            # Separates operand1 and operand2 based on the index of the operator
             n = i.index("-")
             num1 = int(i[0:n])
             num2 = int(i[n + 1:])
+            # Prints the equation
             print(' ', num1, '\n', '-', num2, '\n______\n ', num1 - num2, "\n", sep='')
-            # return " "
+
+
 
     return " "
 
-formula = arithmetic_arranger(["1500 +50", "320-5", "7-6"])
+formula = arithmetic_arranger(['2*2'])
 print(formula)
 # formula2 = arithmetic_arranger(["320 - 5"])
 # print(formula2)
