d19637bbfdf4fcdeeeb67cbc05e42621c816f253
print-hellochris <print.christopherguimaraes@gmail.com>
2022-10-22 17:02:17 -0300

    Changed arithmetic_arranger if/else blocks for better readability. Use black PyPI library to format my code.

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,43 +1,74 @@
+''' https://forum.freecodecamp.org/t/scientific-computing-with-python-projects-arithmetic-formatter/562679'''
+
+
 def arithmetic_arranger(self):
 
 
     for i in self:
-        # Removes blank space before continuing
+#--------------------- Removes blank space before continuing--------------------------------------------------------------
         i = i.replace(" ", "")
-        #Checks if there are more than 5 problems to the function
+#---------------------Checks if there are more than 5 problems to the function-----------------------------------------------------------------------------
         if len(self) > 5:
-            print("Error: Too many problems")
+            return "Error: Too many problems"
             break
-        #Checks if there is a division operation in the arguments
-        elif "/" in i:
-            print("Error:Operator must be either '+' or '-' ")
-            break
-        #checks if there is a multiplication operation in the arguments
-        elif "*" in i:
-            print("Error:Operator must be either '+' or '-' ")
-            break
-        #Checks if there is a + operator.
+#---------------------checks if there's a plus operator. I am working only with the Plus operator for now to see if I can get the problems displayed correctly.---------
         elif "+" in i:
             n = i.index("+")
-            # Separates operand1 and operand2 based on the index of the operator
-            num1 = int(i[0:n])
-            num2 = int(i[n + 1:])
-            # Prints the equation
-            print(' ', num1, '\n', '+', num2, '\n______\n ', num1 + num2, "\n", sep='')
-        # Checks if there is a - operator.
+            operator = '+'
+#-------------------- Separates operand1 and operand2 based on the index of the operator------------------------------------------------------------------------------
+            num1 = (i[0:n])
+            num2 = (i[n + 1:])
+            '''
+            Checks the lengths of digits.
+            '''
+            if len(num1)>4:
+                return 'Number {0} cannot be more than four digits'.format(num1)
+                break
+            else:
+                operand_one = int(num1)            
+            if len(num2)>4:
+                return 'Number {0} cannot contain more than four digits'.format(num2)
+                break
+            else:
+                operand_two = int(num2)
+            #Creates a variable result that sums both operands. 
+            result = operand_one+operand_two
         elif "-" in i:
-            # Separates operand1 and operand2 based on the index of the operator
             n = i.index("-")
-            num1 = int(i[0:n])
-            num2 = int(i[n + 1:])
-            # Prints the equation
-            print(' ', num1, '\n', '-', num2, '\n______\n ', num1 - num2, "\n", sep='')
+            operator = '-'
+#-------------------- Separates operand1 and operand2 based on the index of the operator------------------------------------------------------------------------------
+            num1 = (i[0:n])
+            num2 = (i[n + 1:])
+                      
+            if len(num1)>4:
+                return 'Number {0} cannot be more than four digits'.format(num1)
+                break
+            else:
+                operand_one = int(num1)            
+            if len(num2)>4:
+                return 'Number {0} cannot contain more than four digits'.format(num2)
+                break
+            else:
+                operand_two = int(num2)
+                #Creates a variable result that sums both operands. 
+                result = operand_one-operand_two
+        else:         
+            
+            return "Error:Operator in {0} must be either '+' or '-' .format(i)"
+            break
+        
+        
+        if len(num2) > len(num1):
+            
+            left_margin = ' '*len(num2)
 
-
-
-    return " "
-
-formula = arithmetic_arranger(['2*2'])
+        line_One = left_margin + num1
+        output = '{line_One}\n{line_Two}\n{dashLine}\n{line_Three}'.format(line_One=line_One,line_Two=line_Two,dashLine=dashLine,line_Three=line_Three)
+        return result
+    #I added this empty string because the function kept returning None.
+    
+    
+formula = arithmetic_arranger(['1+2'])
 print(formula)
 # formula2 = arithmetic_arranger(["320 - 5"])
 # print(formula2)
--- nameGenerator.py
+++ nameGenerator.py
@@ -1,65 +1,94 @@
-'''
+"""
 Este programa gera um nome automático com base nas três primeiras letras do seu primeiro nome e as três últimas do seu Sobrenome
 
-'''
+"""
 
-print('------------------------------------------------------------------------------------------------------------------------------------------------------\nBem vindo ao gerador de nomes \nEste Programa gera um nome fantasia com base nas três primeiras letra do '
-      'seu nome e nas três últimas do seu sobrenome. \nDepois te dá um título com base na cor escolhida.\n'
-      'O Sufixo depende do gênero escolhido: Or (Masculino), Si (Feminino) ou Us (Outro) '
-      '\nSiga as instruções abaixo \n ------------------------------------------------------------------------------------------------------------------------------------------------------')
+print(
+    "------------------------------------------------------------------------------------------------------------------------------------------------------\nBem vindo ao gerador de nomes \nEste Programa gera um nome fantasia com base nas três primeiras letra do "
+    "seu nome e nas três últimas do seu sobrenome. \nDepois te dá um título com base na cor escolhida.\n"
+    "O Sufixo depende do gênero escolhido: Or (Masculino), Si (Feminino) ou Us (Outro) "
+    "\nSiga as instruções abaixo \n ------------------------------------------------------------------------------------------------------------------------------------------------------"
+)
 
-print('------------------------------------------------------------------------------------------------------------------------------------------------------')
-gender = input('Qual é seu gênero?\n Masculino \n Feminino \n Outro \n Digite sua resposta: ')
-capitalize(gender)
+print(
+    "------------------------------------------------------------------------------------------------------------------------------------------------------"
+)
+gender = input(
+    "Qual é seu gênero?\n Masculino \n Feminino \n Outro \n Digite sua resposta: "
+)
+
 print(gender)
-if gender == 'Feminino':
-    suffix = 'si'
+gender =gender.capitalize()
+if gender == "Feminino":
+    suffix = "si"
 
-elif gender == 'Masculino':
-    suffix = 'or'
+elif gender == "Masculino":
+    suffix = "or"
 else:
-    suffix = 'us'
-print('------------------------------------------------------------------------------------------------------------------------------------------------------')
+    suffix = "us"
+print(
+    "------------------------------------------------------------------------------------------------------------------------------------------------------"
+)
 
-firstName = input('Digite seu nome: ')
-lastName = input('Digite seu sobrenome: ')
-print('------------------------------------------------------------------------------------------------------------------------------------------------------')
+firstName = input("Digite seu nome: ")
+firstName = firstName.capitalize()
+lastName = input("Digite seu sobrenome: ")
+lastName = lastName.capitalize()
+print(
+    "------------------------------------------------------------------------------------------------------------------------------------------------------"
+)
 
 part_One = firstName[0:3]
 length = len(lastName)
-partTwo = lastName[length - 3: length]
+partTwo = lastName[length - 3 : length]
 
-print('Gerando novo nome ...')
-print('Atualizando dados ...')
+print("Gerando novo nome ...")
+print("Atualizando dados ...")
 
 generatedName = part_One + partTwo
 
-print('------------------------------------------------------------------------------------------------------------------------------------------------------')
-favouriteColor = input('Qual é sua cor preferida: \nAzul \nRoxo \nVerde \nVermelho \nAmarelo \nRosa  \nDigite sua Resposta ')
+print(
+    "------------------------------------------------------------------------------------------------------------------------------------------------------"
+)
+favouriteColor = input(
+    "Qual é sua cor preferida: \nAzul \nRoxo \nVerde \nVermelho \nAmarelo \nRosa  \nDigite sua Resposta "
+)
+favouriteColor = favouriteColor.capitalize()
 
-
-if favouriteColor == 'Azul':
-    Title = 'The Sailor'
-elif favouriteColor == 'Roxo':
-    Title = 'The Royal'
-elif favouriteColor == 'Verde':
-    Title = 'The Ranger'
-elif favouriteColor == 'Vermelho':
-    Title = 'The thief'
-elif favouriteColor == 'Amarelo':
-    Title = 'The Mad'
-elif favouriteColor == 'Rosa':
-    Title = 'The Young'
-elif favouriteColor == 'Preto':
-    Title = 'The seducer of brides'
+if favouriteColor == "Azul":
+    Title = "The Sailor"
+elif favouriteColor == "Roxo":
+    Title = "The Royal"
+elif favouriteColor == "Verde":
+    Title = "The Ranger"
+elif favouriteColor == "Vermelho":
+    Title = "The thief"
+elif favouriteColor == "Amarelo":
+    Title = "The Mad"
+elif favouriteColor == "Rosa":
+    Title = "The Young"
+elif favouriteColor == "Preto":
+    Title = "The seducer of brides"
 else:
-    Title = 'The nobody'
+    Title = "The nobody"
 
 
-print('------------------------------------------------------------------------------------------------------------------------------------------------------ \n '
-      'Opção 1 - Seu novo nome é' + ' ' + generatedName+suffix + ' ' + Title +
-      '\n------------------------------------------------------------------------------------------------------------------------------------------------------')
-print('------------------------------------------------------------------------------------------------------------------------------------------------------ \n '
-      'Opção 2 - Seu novo nome é' + ' ' + generatedName + ' ' + Title +
-      '\n------------------------------------------------------------------------------------------------------------------------------------------------------')
-
+print(
+    "------------------------------------------------------------------------------------------------------------------------------------------------------ \n "
+    "Opção 1 - Seu novo nome é"
+    + " "
+    + generatedName
+    + suffix
+    + " "
+    + Title
+    + "\n------------------------------------------------------------------------------------------------------------------------------------------------------"
+)
+print(
+    "------------------------------------------------------------------------------------------------------------------------------------------------------ \n "
+    "Opção 2 - Seu novo nome é"
+    + " "
+    + generatedName
+    + " "
+    + Title
+    + "\n------------------------------------------------------------------------------------------------------------------------------------------------------"
+)
